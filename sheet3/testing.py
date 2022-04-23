a08d272cb941b29741ef2c3bcc4941efba6d8e53
Leonhard-Moske <leonhard.moske@gmail.com>
2022-04-23 13:18:38 +0200

    finished sheet3

--- sheet3/testing.py
+++ sheet3/testing.py
@@ -6,3 +6,10 @@ array_to_check = np.array([1., 2.])
 is_in_list = np.any(np.all(array_to_check == list_np_arrays, axis=1))
 
 print(is_in_list)
+
+array_to_check2 = np.array([1., 2.])
+list_np_arrays = np.array([1., 1.])
+array_to_check = np.array([1., 2.])
+
+np.equal(array_to_check,array_to_check2)
+print(np.equal(array_to_check,array_to_check2))
