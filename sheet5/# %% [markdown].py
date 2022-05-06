7257da8332b438a40fe55393c1f962ed85203495
Leonhard-Moske <leonhard.moske@gmail.com>
2022-05-06 12:45:13 +0200

    new basis ham rdy

--- sheet5/# %% [markdown].py
+++ sheet5/# %% [markdown].py
@@ -72,6 +72,26 @@ def totalZ(state, L):
         totalZ += Sz(1, state, i)[0]
     return totalZ
 
+def splitBasisTotZ(Basis, L):
+    splitBasis= []
+    stot = []
+    sortedBasis = sorted(Basis, key = lambda state : (totalZ(state, L)))
+    tmpBasis = []
+    oldz = totalZ(sortedBasis[0],L)
+    for state in sortedBasis:
+        z = totalZ(state, L)
+        if z != oldz:
+            splitBasis.append(tmpBasis)
+            stot.append(totalZ(state, L))
+            tmpBasis = [state]
+            oldz = z
+        else:
+            tmpBasis.append(state)
+            oldz = z
+    splitBasis.append(tmpBasis)
+    stot.append(totalZ(state, L))
+    return splitBasis, stot
+
 def totalZmatrix(Basis, L):# calculate the basis by calculating the hamiltonian with all combination from the basis
     Matrix = np.zeros((len(Basis),len(Basis))) # since we know that the basisstates are eigenstates of totalZ we only have to compute the diagonal 
     for i in range(len(Basis)):
@@ -79,7 +99,7 @@ def totalZmatrix(Basis, L):# calculate the basis by calculating the hamiltonian 
     return Matrix
 
 def transition(state): # numpy array
-    newState = np.roll(state)
+    newState = np.roll(state,1)
     return newState
 
 def genFamily(state, operator, L): # generate the family of a state with an operator
@@ -91,22 +111,26 @@ def genFamily(state, operator, L): # generate the family of a state with an oper
 
 def get_representativ(state, operator, L):
     family = genFamily(state, operator, L)
+    tmp = np.copy(family)
     for i in range(len(family[0])): # over the length of the states
        family = family[family[:,i].argsort()]
-    return family[0]
+    for i, s in enumerate(tmp):
+        if np.all(s == family[0] ):
+            return family[0] ,i
+    return family[0] 
 
 def get_norm(state, nk,operator, L ):
     k=2.*np.pi*nk/L
     fam = genFamily(state,operator,L)
 
     different_states = set()
-    for s,r,sign in fam:
+    for s in fam:
         different_states.add(tuple(s))
     
     prefactors = {s:0.0 for s in different_states}
     
-    for s, r, sign in fam:
-        prefactors[tuple(s)] += sign*np.exp(1.j*k*r)
+    for r, s, in enumerate(fam):
+        prefactors[tuple(s)] += np.exp(1.j*k*r)
     
     norm = 0.0
     for s in prefactors:
@@ -114,16 +138,110 @@ def get_norm(state, nk,operator, L ):
         
     return np.sqrt(norm)
 
+def calcHam(basis, L,operator):
+    hamils = []
+    reps={}
+
+    # Create set of representative states, including family size
+    for s in basis:
+        rep,fam = get_representativ(s,operator,L)[0], genFamily(s,operator,L)[0]
+        reps[tuple(rep)] = len(np.unique(fam, axis = 0))
+
+    dim_sum=0
+
+    for nk in range(L):
+        nk = nk  # momentum sector
+        k=2.*np.pi*nk/L
+        sector_reps={}
+        
+        index = 0
+        for rep in reps.keys():
+            norm = get_norm(rep,nk, operator, L)
+            if abs(norm)>1e-6:
+                sector_reps[tuple(rep)] = (norm, index) # for each representative state, we need to get the norm of its corresponding momentum state, and its index in our basis order
+                index+=1
+        dim=len(list(sector_reps))
+        dim_sum+=dim
+        
+        print("Got momentum sector nk={} of size {}".format(nk, dim))
+        
+        Hk = np.zeros( (dim,dim) , dtype=np.complex128 )
+        for rep in sector_reps.keys():
+            
+            norm, index = sector_reps[rep]
+            matel=0.0
+            ## diagonal terms
+            for site in range(L):
+                matel+=rep[site]*rep[(site+1)%L]
+            Hk[index,index]=matel
+                    
+                
+            
+            # offdiagonal matrix elements
+            for site in range(L):
+                if rep[site]!=rep[(site+1)%L]:
+                    state2=list(rep)
+                    state2[site], state2[(site+1)%L] =  state2[(site+1)%L], state2[site]
+                    rep2, num_translations_to_get_rep = get_representativ(state2, operator, L)
+                    #print(state2,rep2)
+                    
+                    if tuple(rep2) in sector_reps:
+                        norm2, idx2 = sector_reps[tuple(rep2)]
+                        matel = 0.5*norm2/norm*np.exp(-1.j*k*num_translations_to_get_rep)
+                        Hk[index,idx2]+=matel
+        hamils.append(Hk)
+    return hamils
+    
+
 # %%
-testindex = 6
+testindex = 9
 
-L = 4
+L = 5
 
-a = genBasis(L)
+a = genBasis(L,0.5)
 print(Splus(1,np.copy(a[testindex]), 1))
 print(a[testindex], len(a))
 
 print(transition(a[testindex]))
+
+
+
+# %%
+
+print(genFamily(a[testindex],transition,L), "RESULT")
+print(get_representativ(a[testindex],transition,L))
+print(get_norm([0.5,-0.5,0.5,-0.5],2,transition,L))
+
+
+# %%
+import sympy
+sympy.Matrix(calcHam(genBasis(L,0.5),L,transition)[1])
+
+# %%
+dimsum = 0
+L = 5
+for nk in range(L):
+    sector_reps={}
+    for s in genBasis(L,0.5):
+        rep , xl = get_representativ(s,transition,L)
+        #fam = genFamily(rep)
+        norm=get_norm(rep,nk,transition,L)
+        if norm>1e-6: 
+            if tuple(rep) not in sector_reps:
+                sector_reps[tuple(rep)]=norm
+    secdim = len(sector_reps.keys())
+    print(nk, secdim)
+    dimsum+=secdim
+print("total ", dimsum, " should be ", 2**L)
+
+# %%
+splitBasis, sZtot = splitBasisTotZ(genBasis(L,0.5),L)
+if L%2 == 0:
+    ind = sZtot.index(0)
+    testham = calcHam(splitBasis[ind],L,transition)[1]
+else:
+    ind = sZtot.index(0.5)
+    testham = calcHam(splitBasis[ind],L,transition)[1]
 
 # %%
 import numpy as np
@@ -149,7 +267,7 @@ vec = np.asarray([1,2,3,4])
 
 print(matrixVectorproduct(matrix,vec))
 
-L = 9
+L = 2
 
 ham = calcMatrix(genBasis(L,0.5),1,L,0.5)
 print(csr(ham))
@@ -185,8 +303,8 @@ def lancoz(A, v_un, m): #A is scipy sparse matrix
         v[j+1] = w/beta[j+1]
     return v, alpha, beta[1:]
 
-L = 8
-m = 8
+L = 2
+m = 2
 
 mat = csr_matrix(calcMatrix(genBasis(L,0.5),1,L,0.5))
 H = calcMatrix(genBasis(L,0.5),1,L,0.5)
@@ -206,5 +324,23 @@ eigenT = np.linalg.eigvals(T)
 eigenH = np.linalg.eigvals(H)
 
 #print(np.unique(eigenH), eigenT)
+
+# %%
+mat = csr_matrix(testham)
+v0 = np.random.rand(2**L)
+m = 4
+v , alpha, beta = lancoz(mat,v0,m)
+
+
+T = np.zeros((m,m))
+
+for i in range(m):
+    T[i,i] = alpha[i]
+for i in range(m-1):
+    T[i,i+1] = beta[i]
+    T[i+1,i] = beta[i]
+
+eigenT = np.linalg.eigvals(T)
+print(eigenT)
 
 
